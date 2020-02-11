dfaa1a1cdd9d9164ccce432f71d240c25567596b
/SabrinaRey <sabrinareynares@gmail.com>
2020-02-11 19:09:47 -0300

    agregue estilos y temp en js

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 fetch('http://api.openweathermap.org/data/2.5/weather?q=cordoba&units=metric&appid=4f31e259f539d016ea813c108c431829')
-    .then (res => res.json())
-    .then (info => {
+    .then(res => res.json())
+    .then(info => {
         console.log(info)
-            
-            const cityName = info.name;
-            const description = info.weather[0].description;
-            const img = " https://openweathermap.org/img/wn/03n.png"
-            const precipitation =info.clouds.all;
-            const humidity= info.main.humidity;
-            const wind =info.wind.speed
 
-        ;
+        const cityName = info.name;
+        const description = info.weather[0].description;
+        const temperatura = info.main.temp
+        const img = " https://openweathermap.org/img/wn/03n.png"
+        const precipitation = info.clouds.all;
+        const humidity = info.main.humidity;
+        const wind = info.wind.speed
+
+            ;
 
 
 
@@ -18,25 +19,24 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=cordoba&units=metric&app
         const contenedor = document.querySelector("#contenedor")
 
         contenedor.innerHTML = `
-            <div>
-            <div>
+            
+            <div id ="ciudadDesc">
             <h1>${cityName} </h1>
             <p>${description} </p>
-         </div>
-         <div>             
-            <img src="${img}">
             </div>
-            <div>
+            <div id ="imageIcon">             
+                  <img src="${img}">
+                  <h2>${temperatura}</h2>
+            </div>
+         
+            <div id ="ciudadDetalles">
             <p> Lluvias:${precipitation}% </p>
             <p>Humedad:${humidity}% </p>
             <p> Vientos:${wind} m/s </p>
             </div>
-        
-            
-            
-            </div>`
+            `
     })
-    
 
- 
+
+
 
