0bcce13e4b571c79b47d0cb5b6002c2c78c88339
/SabrinaRey <sabrinareynares@gmail.com>
2020-02-17 22:01:48 -0300

    agregamos pronostico extendido

--- main.js
+++ main.js
@@ -7,49 +7,99 @@ form.onsubmit = e => {
 
 }
 
+
 const buscarClimaPorCiudad = (ciudad) =>{
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=4f31e259f539d016ea813c108c431829`)
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&lang=es&appid=9f39cdba4d4a89d545134347942f077d`)
     .then(res => res.json())
     .then(info => {
-        console.log(info)
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${ciudad}&units=metric&lang=es&appid=9f39cdba4d4a89d545134347942f077d`)
+            .then(res => res.json())
+            .then(datosPorHora => {
+                console.log(info)
+                console.log(datosPorHora)
 
-        const cityName = info.name;
-        const description = info.weather[0].description;
-        const temperatura = info.main.temp
-        const img = " https://openweathermap.org/img/wn/03n.png"
-        const precipitation = info.clouds.all;
-        const humidity = info.main.humidity;
-        const wind = info.wind.speed
+                const cityName = info.name;
+                const description = info.weather[0].description;
+                const temperatura = info.main.temp
+                const img = `https://openweathermap.org/img/wn/${info.weather[0].icon}.png`
+                const precipitation = info.clouds.all;
+                const humidity = info.main.humidity;
+                const wind = info.wind.speed;
+        
+                const contenedorHora = document.getElementById("contenedorPorHora")
+                
+                let acumuladora = ""
+        
+                datosPorHora.list.forEach((dato, i) => {
+        
+                const descriptionPorHora = datosPorHora.list[i].weather[0].description;
+                const temperaturaPorHora = datosPorHora.list[i].main.temp
+                const imgPorHora = `https://openweathermap.org/img/wn/${datosPorHora.list[i].weather[0].icon}.png`
+                const precipitationPorHora = datosPorHora.list[i].clouds.all;
+                const humidityPorHora = datosPorHora.list[i].main.humidity;
+                const windPorHora = datosPorHora.list[i].wind.speed;
+                const fechaYHora = datosPorHora.list[i].dt_txt;
+              
 
-            ;
+                    
+                    acumuladora += `
+                    <div id ="secundario">
+                    <div id ="ciudadDesc">
+                    <h2 id ="titulodeCiudad">${cityName} </h2>
+                    <p id="fechaYHora">${fechaYHora}</p>
+                    <p id ="descriptionTotal">${descriptionPorHora} </p>
+                    </div>
+                    <div id="contenedorDetalles">
+                    <div id ="imageIcon">             
+                          <img src="${imgPorHora}">
+                          <h2>${temperaturaPorHora}</h2><span>°C</span>
+                    </div>
+                    <div id ="ciudadDetalles">
+                    <p> Lluvias:${precipitationPorHora}% </p>
+                    <p>Humedad:${humidityPorHora}% </p>
+                    <p> Vientos:${windPorHora} m/s </p>
+                    </div>
+                    </div>
+                    </div>
+                    `
+        
+                })
+                
+                contenedorHora.innerHTML = acumuladora      
+
+                const contenedor = document.querySelector("#contenedor")
+
+                contenedor.innerHTML = `
+        
+                <div id ="principal">
+                    <div id ="ciudadDesc">
+                    <h2 id ="titulodeCiudad">${cityName} </h2>
+                    <p id ="descriptionTotal">${description} </p>
+                    </div>
+                    <div id="contenedorDetalles">
+                    <div id ="imageIcon">             
+                          <img src="${img}">
+                          <h2>${temperatura}</h2><span>°C</span>
+                    </div>
+                    <div id ="ciudadDetalles">
+                    <p> Lluvias:${precipitation}% </p>
+                    <p>Humedad:${humidity}% </p>
+                    <p> Vientos:${wind} m/s </p>
+                    </div>
+                    </div>
+                    </div>`
+
+
+            })
+
+           
 
 
 
 
-        const contenedor = document.querySelector("#contenedor")
-
-        contenedor.innerHTML = `
-            
-        <div id ="principal">
-            <div id ="ciudadDesc">
-            <h2 id ="titulodeCiudad">${cityName} </h2>
-            <p id ="descriptionTotal">${description} </p>
-            </div>
-            <div id="contenedorDetalles">
-            <div id ="imageIcon">             
-                  <img src="${img}">
-                  <h2>${temperatura}</h2><span>°C</span>
-            </div>
-            <div id ="ciudadDetalles">
-            <p> Lluvias:${precipitation}% </p>
-            <p>Humedad:${humidity}% </p>
-            <p> Vientos:${wind} m/s </p>
-            </div>
-            </div>
-            </div>`
+       
     })
 }
-
 
 
